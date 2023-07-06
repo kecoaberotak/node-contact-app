84f0612f631232608f7f3101d5125a89fb8f5b66
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-07-06 20:55:27 +0700

    tambah command tampil list kontak

--- app.js
+++ app.js
@@ -1,33 +1,43 @@
-const yargs = require('yargs');
-const simpanContact = require('./contacts');
+const yargs = require("yargs");
+const contacts = require("./contacts");
 
 // syntax yargs
 // .command(cmd, desc, [builder], [handler])
 
-
 // Versi 2 - pake yargs
+yargs
+  .command({
+    command: "add",
+    describe: "Menambahkan contact",
+    builder: {
+      nama: {
+        describe: "Nama pengguna",
+        demandOption: true,
+        type: "string",
+      },
+      email: {
+        describe: "Alamat email pengguna",
+        demandOption: false,
+        type: "string",
+      },
+      noHp: {
+        describe: "Nomor handphone pengguna",
+        demandOption: true,
+        type: "string",
+      },
+    },
+    handler: function (argv) {
+      contacts.simpanContact(argv.nama, argv.email, argv.noHp);
+    },
+  })
+  .demandCommand(); // demandCommand() => mengahruskan kita pas jalanin ngasih setidaknya satu command
+
+// menampilkan daftar nama dan nomor
 yargs.command({
-  command   : 'add',
-  describe  : 'Menambahkan contact',
-  builder   : {
-    nama  : {
-      describe      : 'Nama pengguna',
-      demandOption  : true,
-      type          : 'string'
-    },
-    email  : {
-      describe      : 'Alamat email pengguna',
-      demandOption  : false,
-      type          : 'string'
-    },
-    noHp  : {
-      describe      : 'Nomor handphone pengguna',
-      demandOption  : true,
-      type          : 'string'
-    }
-  },
-  handler : function (argv) {
-    simpanContact(argv.nama, argv.email, argv.noHp);
+  command: "list",
+  describe: "menampilkan nama dan nomor Hp",
+  handler() {
+    contacts.listContacts();
   },
 });
 
--- contacts.js
+++ contacts.js
@@ -1,53 +1,68 @@
-const fs = require('node:fs');
-const chalk = require('chalk');
-const validator = require('validator');
+const fs = require("node:fs");
+const chalk = require("chalk");
+const validator = require("validator");
 
 // check directory dan membuat folder data
-const dirPath = './data';
-if(!fs.existsSync(dirPath)){
+const dirPath = "./data";
+if (!fs.existsSync(dirPath)) {
   fs.mkdirSync(dirPath);
-};
+}
 
 // check dan membuat file contact.json
-const filePath = './data/contact.json';
-if(!fs.existsSync(filePath)){
-  fs.writeFileSync(filePath, '[]', 'utf-8');
+const filePath = "./data/contact.json";
+if (!fs.existsSync(filePath)) {
+  fs.writeFileSync(filePath, "[]", "utf-8");
+}
+
+// load file JSON
+const loadContacts = () => {
+  const fileBuffer = fs.readFileSync("./data/contact.json", "utf-8");
+  const contacts = JSON.parse(fileBuffer);
+  return contacts;
 };
 
-
+// simpan contact ke json
 const simpanContact = (nama, email, noHp) => {
   // karena key dan valuenya sama, nulisnya gini
-  if(email === undefined){}
-  const contact = {nama , email, noHp};
-  const bacaFile = fs.readFileSync('./data/contact.json', 'utf-8');
-
-  const contacts = JSON.parse(bacaFile);
+  if (email === undefined) {
+  }
+  const contact = { nama, email, noHp };
+  const contacts = loadContacts();
 
   // Cek duplikasi
-  const duplikat = contacts.find(contact => contact.nama === nama);
-  if(duplikat){
-    console.log(chalk.red.bold.inverse('Nama sudah terdaftar, gunakan nama lain!!!'));
+  const duplikat = contacts.find((contact) => contact.nama === nama);
+  if (duplikat) {
+    console.log(chalk.red.bold.inverse("Nama sudah terdaftar, gunakan nama lain!!!"));
     return false;
-  };
+  }
 
   // cek email
-  if(email != undefined){
-    if(!validator.isEmail(email)){
-      console.log(chalk.red.bold.inverse('Gunakan email yang valid!!!'));
+  if (email != undefined) {
+    if (!validator.isEmail(email)) {
+      console.log(chalk.red.bold.inverse("Gunakan email yang valid!!!"));
       return false;
     }
   }
 
   // cek nomor hp indo
-  if(!validator.isMobilePhone(noHp, 'id-ID')){
-    console.log(chalk.red.bold.inverse('Gunakan nomor telepon Indonesia!!!'));
+  if (!validator.isMobilePhone(noHp, "id-ID")) {
+    console.log(chalk.red.bold.inverse("Gunakan nomor telepon Indonesia!!!"));
     return false;
   }
 
   contacts.push(contact);
 
-  fs.writeFileSync('./data/contact.json', JSON.stringify(contacts));
-  console.log(chalk.green.bold.inverse('Terima kasih sudah memasukkan data anda'));
+  fs.writeFileSync("./data/contact.json", JSON.stringify(contacts));
+  console.log(chalk.green.bold.inverse("Terima kasih sudah memasukkan data anda"));
 };
 
-module.exports = simpanContact;
+// Tampil list nama dan nomor hp
+const listContacts = () => {
+  const contacts = loadContacts();
+  console.log(chalk.bold.green("Daftar Kontak: \n"));
+  contacts.forEach((contact, index) => {
+    console.log(`${index + 1}. Nama : ${contact.nama} \n   Nomor Handphone : ${contact.noHp} `);
+  });
+};
+
+module.exports = { simpanContact, listContacts };
