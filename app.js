83ce2ee0c3008ea302863c8610af416ef4d799fb
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-07-06 16:14:21 +0700

    refactoring

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
-const contacts = require('./contacts');
+// mengambil argument dari command line
 
-const main = async () => {
-  const nama = await contacts.pertanyaan('Nama');
-  const email = await contacts.pertanyaan('Email');
-  const noHp = await contacts.pertanyaan('Nomor Handphone');
 
-  contacts.simpanContact(nama, email, noHp);
-};
 
-main();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Versi 1
+// const contacts = require('./contacts');
+
+// const main = async () => {
+//   const nama = await contacts.pertanyaan('Nama');
+//   const email = await contacts.pertanyaan('Email');
+//   const noHp = await contacts.pertanyaan('Nomor Handphone');
+
+//   contacts.simpanContact(nama, email, noHp);
+// };
+
+// main();
