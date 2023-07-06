821cef6d483257b4b317a9d9fd89641ecee84408
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-07-06 17:04:14 +0700

    berhasil ubah fungsi pertanyaan pake yargs

--- app.js
+++ app.js
@@ -1,33 +1,52 @@
-// mengambil argument dari command line
-// console.log(process.argv);
+const yargs = require('yargs');
+const contacts = require('./contacts');
 
-// pake yargs
+// syntax yargs
+// .command(cmd, desc, [builder], [handler])
 
-
-
-
-
-
-
-
+// cara 1
+// yargs.command('add', 'Menambahkan contact baru', () => {}, (argv) => {
+//   console.log(argv.nama);
+// });
 
 
+// Versi 2
+yargs.command({
+  command   : 'add',
+  describe  : 'Menambahkan contact',
+  builder   : {
+    nama  : {
+      describe      : 'Nama pengguna',
+      demandOption  : true,
+      type          : 'string'
+    },
+    email  : {
+      describe      : 'Alamat email pengguna',
+      demandOption  : false,
+      type          : 'string'
+    },
+    noHp  : {
+      describe      : 'Nomor handphone pengguna',
+      demandOption  : true,
+      type          : 'string'
+    }
+  },
+  handler : function (argv) {
+    contacts.simpanContact(argv.nama, argv.email, argv.noHp);
+  },
+});
 
-
-
-
-
+// buat jalanin
+yargs.parse();
 
 
 // Versi 1
-// const contacts = require('./contacts');
+const main = async () => {
+  const nama = await contacts.pertanyaan('Nama');
+  const email = await contacts.pertanyaan('Email');
+  const noHp = await contacts.pertanyaan('Nomor Handphone');
 
-// const main = async () => {
-//   const nama = await contacts.pertanyaan('Nama');
-//   const email = await contacts.pertanyaan('Email');
-//   const noHp = await contacts.pertanyaan('Nomor Handphone');
+  contacts.simpanContact(nama, email, noHp);
+};
 
-//   contacts.simpanContact(nama, email, noHp);
-// };
-
-// main();
+main();
