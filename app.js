cb0b0488a79a95d4ed9c54cf25a3fb767625f7ba
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-07-06 15:38:12 +0700

    cobain pake node.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const contacts = require('./contacts');
+
+const main = async () => {
+  const nama = await contacts.pertanyaan('Nama');
+  const email = await contacts.pertanyaan('Email');
+  const noHp = await contacts.pertanyaan('Nomor Handphone');
+
+  contacts.simpanContact(nama, email, noHp);
+};
+
+main();
